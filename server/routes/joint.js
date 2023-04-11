f382d0cc94fc784db3d5514a358a06e70d263ebe
Yihong Wang <yh.wang@ibm.com>
2023-04-11 11:09:43 -0700

    connect to serial port
    hardcoded to `/dev/ttyACM0` for now

--- server/routes/joint.js
+++ server/routes/joint.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
+var port = new SerialPort({
+   path: '/dev/ttyACM0',
+   baudRate: 115200,
+   parity: 'N',
+   dataBits: 8,
+   stopBits: 1,
+   rtscts: false,
+   hupcl: false,
+});
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+parser.on('data', data =>{
+  console.log(`Res: ${data}`);
+});
 
 /* GET home page. */
 router.post('/turn', (req, res) => {
   if (req.query.joint && req.query.direction && req.query.degree) {
-    console.log(`turn joint: ${req.query.joint}, direction: ${req.query.direction}, degree: ${req.query.degree}`)
-    res.send({"res" : "ok"});
+    port.write(`/${req.query.joint}/MOVE/${req.query.direction}/${req.query.degree}\n`);
+    port.drain(err => {
+      if (err) {
+        res.send({"res": "failed"});
+      } else {
+        res.send({"res" : "ok"});
+      }
+    });
   } else {
     res.send({"res": "failed"});
   }
@@ -13,8 +35,14 @@ router.post('/turn', (req, res) => {
 
 router.post('/run', (req, res) => {
   if (req.query.joint && req.query.direction) {
-    console.log(`run joint: ${req.query.joint}, direction: ${req.query.direction}`);
-    res.send({"res": "ok"});
+    port.write(`/${req.query.joint}/GO/${req.query.direction}\n`);
+    port.drain(err => {
+      if (err) {
+        res.send({"res": "failed"});
+      } else {
+        res.send({"res" : "ok"});
+      }
+    });
   } else {
     res.send({"res": "failed"})
   }
@@ -22,8 +50,14 @@ router.post('/run', (req, res) => {
 
 router.post('/stop', (req, res) => {
   if (req.query.joint) {
-    console.log(`stop joint: ${req.query.joint}`);
-    res.send({"res": "ok"});
+    port.write(`/${req.query.joint}/STOP\n`);
+    port.drain(err => {
+      if (err) {
+        res.send({"res": "failed"});
+      } else {
+        res.send({"res" : "ok"});
+      }
+    });
   } else {
     res.send({"res": "failed"})
   }
